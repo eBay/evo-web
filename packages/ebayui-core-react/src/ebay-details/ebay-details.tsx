30eb7c0db2504e32b75b95ced58499cb4acbe328
HenriqueLimas <henrique.ramos.limas@gmail.com>
2025-05-29 14:37:09 -0700

    chore(react): use Prettier for formatting files

--- packages/ebayui-core-react/src/ebay-details/ebay-details.tsx
+++ packages/ebayui-core-react/src/ebay-details/ebay-details.tsx
@@ -1,50 +1,51 @@
-import React, { ComponentProps, ElementType, FC } from 'react'
-import classnames from 'classnames'
-import { EbayEventHandler } from '../events'
-import { EbayIcon } from '../ebay-icon'
+import React, { ComponentProps, ElementType, FC } from "react";
+import classnames from "classnames";
+import { EbayEventHandler } from "../events";
+import { EbayIcon } from "../ebay-icon";
 
 type ToggleProps = {
     open: boolean;
-}
+};
 
-export type EbayDetailsProps = Omit<ComponentProps<'details'>, 'onToggle'> & {
-    text: string
-    className?: string
-    size?: 'regular' | 'small'
-    alignment?: 'regular' | 'center'
-    layout?: 'regular' | 'accordion'
-    as?: ElementType
-    onToggle?: EbayEventHandler<HTMLDetailsElement, ToggleProps>
-}
+export type EbayDetailsProps = Omit<ComponentProps<"details">, "onToggle"> & {
+    text: string;
+    className?: string;
+    size?: "regular" | "small";
+    alignment?: "regular" | "center";
+    layout?: "regular" | "accordion";
+    as?: ElementType;
+    onToggle?: EbayEventHandler<HTMLDetailsElement, ToggleProps>;
+};
 
 const EbayDetails: FC<EbayDetailsProps> = ({
     size,
     alignment,
     layout,
     text,
-    as: Component = 'div',
+    as: Component = "div",
     className,
     open,
     onToggle,
     children,
-    ...rest }: EbayDetailsProps) => {
-    const handleToggle = onToggle && ((event: React.SyntheticEvent<HTMLDetailsElement>) => {
-        onToggle(event, { open: event.currentTarget.open })
-    })
+    ...rest
+}: EbayDetailsProps) => {
+    const handleToggle =
+        onToggle &&
+        ((event: React.SyntheticEvent<HTMLDetailsElement>) => {
+            onToggle(event, { open: event.currentTarget.open });
+        });
 
     return (
-        <details
-            open={open}
-            onToggle={handleToggle}
-            className={classnames('details', className)}
-            {...rest}>
-            <summary className={classnames('details__summary', {
-                'details__summary--small': size === 'small',
-                'details__summary--center': alignment === 'center'
-            })}>
+        <details open={open} onToggle={handleToggle} className={classnames("details", className)} {...rest}>
+            <summary
+                className={classnames("details__summary", {
+                    "details__summary--small": size === "small",
+                    "details__summary--center": alignment === "center",
+                })}
+            >
                 <span className="details__label">{text}</span>
                 <span className="details__icon" hidden>
-                    {layout === 'accordion' ? (
+                    {layout === "accordion" ? (
                         <>
                             <EbayIcon className="details__expand" name="add16" />
                             <EbayIcon className="details__collapse" name="remove16" />
@@ -54,13 +55,15 @@ const EbayDetails: FC<EbayDetailsProps> = ({
                     )}
                 </span>
             </summary>
-            <Component className={classnames({
-                'details__content': layout === 'accordion'
-            })}>
+            <Component
+                className={classnames({
+                    details__content: layout === "accordion",
+                })}
+            >
                 {children}
             </Component>
         </details>
-    )
-}
+    );
+};
 
-export default EbayDetails
+export default EbayDetails;
