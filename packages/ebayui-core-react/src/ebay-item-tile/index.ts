3c3f4d8d5e448dea0a08587336358d47b83b0aec
Bruno Kern <bruno2kd@gmail.com>
2025-06-10 19:02:07 -0300

    fix: change type name

--- packages/ebayui-core-react/src/ebay-item-tile/index.ts
+++ packages/ebayui-core-react/src/ebay-item-tile/index.ts
@@ -3,6 +3,6 @@ export { default as EbayItemTileSuperTitle } from "./item-tile-super-title";
 export { default as EbayItemTileTitle } from "./item-tile-title";
 export { default as EbayItemTileSubTitle } from "./item-tile-sub-title";
 export { default as EbayItemTileAction } from "./item-tile-action";
-export { default as EbayItemTileDescription, type EbayItemTileSectionDescriptionProps } from "./item-tile-description";
+export { default as EbayItemTileDescription, type EbayItemTileDescriptionProps } from "./item-tile-description";
 
 export * from "./types";
