1f55b4d8bc30417c8093732da3b77486d25f9f44
Andrew Gliga <agliga4@gmail.com>
2025-06-16 13:56:55 -0700

    chore: added visual-html to skin

--- packages/skin/vite.config.js
+++ packages/skin/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { playwright } from "@vitest/browser-playwright";
 export default defineConfig({
     base: process.env.BASE_URL,
     resolve: {
@@ -9,5 +10,29 @@ export default defineConfig({
                 "../../node_modules/@ebay/design-tokens",
             ),
         },
+    },
+    test: {
+        onConsoleLog: () => true,
+        globals: true,
+        projects: [
+            {
+                extends: true,
+                test: {
+                    name: "browser",
+                    browser: {
+                        enabled: true,
+                        provider: playwright(),
+                        headless: true,
+                        instances: [
+                            {
+                                browser: "chromium",
+                            },
+                        ],
+                    },
+                    include: ["src/**/test.{ts,js}"],
+                    setupFiles: ["./test.setup.ts"],
+                },
+            },
+        ],
     },
 });
