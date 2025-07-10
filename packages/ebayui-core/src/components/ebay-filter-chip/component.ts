490c4c71ab03222a42b3debc28d0a8343549fc42
Andrew Gliga <agliga4@gmail.com>
2025-07-10 08:29:43 -0700

    feat(chips): added new components

---
+++ packages/ebayui-core/src/components/ebay-filter-chip/component.ts
@@ -0,0 +1,66 @@
+import type { AttrString } from "marko/tags-html";
+import type { WithNormalizedProps } from "../../global";
+import type { AttrBoolean } from "marko/tags-html";
+
+interface Image extends Marko.HTML.Img {
+    as?: string;
+}
+
+interface FilterChipInput extends Omit<Marko.HTML.Button, `on${string}` | "type">,
+        Omit<Marko.HTML.A, `on${string}`> {
+    renderBody?: Marko.Body;
+    selected?: boolean;
+    variant?: "default" | "expressive" | "menu";
+    disabled?: AttrBoolean;
+    icon?: Marko.AttrTag<{ renderBody?: Marko.Body }>;
+    image?: Marko.AttrTag<Image>;
+    clippedText?: AttrString;
+    expanded?: boolean;
+    "on-click"?: (event: {
+        originalEvent: MouseEvent;
+        expanded: boolean;
+        selected: boolean;
+    }) => void;
+}
+export interface Input extends WithNormalizedProps<FilterChipInput> {}
+
+export interface State {
+    expanded?: boolean;
+    selected?: boolean;
+    loaded: boolean;
+}
+
+class SelectionChip extends Marko.Component<Input, State> {
+    handleButtonClick(originalEvent: MouseEvent) {
+        let expanded = this.state.expanded;
+        let selected = this.state.selected;
+        if (!this.input.disabled) {
+            if (this.input.variant === "menu") {
+                expanded = !expanded;
+                this.state.expanded = expanded;
+            } else {
+                selected = !selected;
+                this.state.selected = selected;
+            }
+            this.emit("click", {
+                expanded,
+                selected,
+                originalEvent,
+            });
+        }
+    }
+
+    onMount() {
+        this.state.loaded = true;
+    }
+
+    onInput(input: Input) {
+        this.state = {
+            selected: input.selected || false,
+            expanded: input.expanded || false,
+            loaded: false
+        };
+    }
+}
+
+export default SelectionChip;
