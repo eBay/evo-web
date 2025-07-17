28ce4cca9a1c384a5601a65a81357c0081dffecc
Caleb Nance <calebnance@gmail.com>
2025-07-17 15:07:00 -0400

    docs: icon examples

---
+++ figma-code-connect/marko/icons/chevron-16.figma.template.js
@@ -0,0 +1,11 @@
+// url=https://www.figma.com/design/wUMu6fcAuDLthwAnCPJ2T6/Evo-Icon-Library?node-id=750-33501
+const figma = require("figma");
+
+export default {
+  example: figma.code`<ebay-chevron-down-16-icon />`,
+  id: "ebay-chevron-down-16-icon",
+  metadata: {
+    // controls how this appears when nested in other components
+    nestable: true,
+  },
+};
