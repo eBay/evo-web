4e3d75379676ec60816d56f9015e6a9ea4eecd2b
LuLaValva <lukelavalva@gmail.com>
2025-10-27 09:59:57 -0700

    fix: run postcss at correct time in build

--- packages/skin/postcss.config.js
+++ packages/skin/postcss.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 
 module.exports = (ctx) => ({
     plugins: [
+        require("postcss-import")({
+            path: ["../../node_modules"],
+        }),
         require("autoprefixer"),
         require("cssnano")({
             preset: [
