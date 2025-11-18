8911faebd8759bbb565a0fee24f92ccae6118595
Andrew Gliga <agliga4@gmail.com>
2025-11-18 12:34:57 -0800

    chore: update vite config

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+export default defineConfig({
+  base: process.env.BASE_URL,
+  resolve: {
+    alias: {
+      "@ebay/design-tokens": resolve(
+        __dirname,
+        "node_modules/@ebay/design-tokens",
+      ),
+    },
+  },
+});
