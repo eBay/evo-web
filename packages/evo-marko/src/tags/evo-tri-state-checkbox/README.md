5511f1c45a7f730d8dfac0cec43534875e00cf60
Andrew Gliga <agliga4@gmail.com>
2025-11-19 12:53:33 -0800

    feat(marko6): added tristate checkbox

---
+++ packages/evo-marko/src/tags/evo-tri-state-checkbox/README.md
@@ -0,0 +1,16 @@
+<h1 style="display: flex; justify-content: space-between; align-items: center;">
+    <span>
+        evo-tri-state-checkbox
+    </span>
+    <span style="font-weight: normal; font-size: medium; margin-bottom: -15px;">
+        DS v1.2.0
+    </span>
+</h1>
+
+A checkbox that toggles from unchecked, to partially checked, to fully checked states. Uses `<input/>` under the hood with custom icons.
+
+## Examples and Documentation
+
+- [Storybook](https://ebay.github.io/evo-web/ebayui-core/?path=/story/form-input-evo-tri-state-checkbox)
+- [Storybook Docs](https://ebay.github.io/evo-web/ebayui-core/?path=/docs/form-input-evo-tri-state-checkbox)
+- [Code Examples](https://github.com/eBay/evo-web/tree/main/packages/ebayui-core/src/components/evo-tri-state-checkbox/examples)
---
+++ packages/evo-marko/src/tags/evo-tri-state-checkbox/tri-state-checkbox.stories.ts
@@ -0,0 +1,89 @@
+import { buildExtensionTemplate } from "../../common/storybook/utils";
+import Readme from "./README.md";
+import Component from "./index.marko";
+import defaultTemplate from "./examples/default.marko";
+import defaultCode from "./examples/default.marko?raw";
+import mixedImplementationTemplate from "./examples/mixed-implementation.marko";
+import mixedImplementationCode from "./examples/mixed-implementation.marko?raw";
+import WithLabelTemplate from "./examples/with-label.marko";
+import DisabledTemplate from "./examples/disabled-with-label.marko";
+import WithLabelCode from "./examples/with-label.marko?raw";
+import DisabledCode from "./examples/disabled-with-label.marko?raw";
+import type { Input } from "./component";
+
+export default {
+  title: "form input/evo-tri-state-checkbox",
+  component: Component,
+  parameters: {
+    docs: {
+      description: {
+        component: Readme,
+      },
+    },
+  },
+
+  argTypes: {
+    checked: {
+      options: ["false", "mixed", "true"],
+      type: { category: "Options" },
+      description:
+        'Either "true", "false" or "mixed". Defaults to "false". Changes the checkbox state to the given one depdending on the checked state.',
+      table: {
+        defaultValue: {
+          summary: "false",
+        },
+      },
+    },
+    skipMixed: {
+      type: "boolean",
+      control: { type: "boolean" },
+      description:
+        "If set, then will skip the mixed toggle when clicking on checkbox. Used if in some cases you want to toggle between all items selected or none.",
+    },
+    size: {
+      options: ["regular", "large"],
+      type: { category: "Options" },
+      description:
+        'Either "large" or "regular". Sets the checkbox icon. Default is regular. For mweb this should be set to large. (Note: The dimensions of the checkbox will not change, but only the icon)',
+      table: {
+        defaultValue: {
+          summary: "regular",
+        },
+      },
+    },
+    onChange: {
+      action: "on-change",
+      description: "Triggered on change",
+      table: {
+        category: "Events",
+        defaultValue: {
+          summary: "{ originalEvent, value, checked }",
+        },
+      },
+    },
+    onFocus: {
+      action: "on-focus",
+      description: "Triggered on focus",
+      table: {
+        category: "Events",
+        defaultValue: {
+          summary: "{ originalEvent, value }",
+        },
+      },
+    },
+  },
+};
+
+export const WithLabel = buildExtensionTemplate(
+  WithLabelTemplate,
+  WithLabelCode,
+);
+
+export const Disabled = buildExtensionTemplate(DisabledTemplate, DisabledCode);
+
+export const mixedImplementation = buildExtensionTemplate(
+  mixedImplementationTemplate,
+  mixedImplementationCode,
+);
+
+export const Isolated = buildExtensionTemplate(defaultTemplate, defaultCode);
