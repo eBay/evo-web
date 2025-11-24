83510e122210bf7e91e06d33e818ecf8348676dc
Andrew Gliga <agliga4@gmail.com>
2025-11-24 13:08:25 -0800

    chore: review comments

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 export default defineConfig({
+  assetsInclude: ["**/*.md"],
   base: process.env.BASE_URL,
   resolve: {
     alias: {
