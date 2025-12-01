19cc7d1d139ae78e5eccf2bd946a6180597bba00
Andrew Gliga <agliga4@gmail.com>
2025-12-01 14:18:57 -0800

    chore: regenerated package-lock

--- packages/ebayui-core/src/components/ebay-video/test/test.browser.js
+++ packages/ebayui-core/src/components/ebay-video/test/test.browser.js
@@ -13,18 +13,12 @@ describe("video", () => {
     beforeEach(async () => {
         component = await render(Default, { "aria-label": "video" });
     });
-    // Shaka is not supported by headless chrome, and will throw 4032 error.
-    // This however, means that the player has loaded correctly
     describe("renders default", () => {
         it("should render", async () => {
             await waitFor(
                 () => {
-                    const data = component.emitted("load-error");
+                    const data = component.emitted("loadstart");
                     expect(data).to.have.length(1);
-                    expect(data[0][0]).toHaveProperty(
-                        "message",
-                        "Shaka Error 4032",
-                    );
                 },
                 { timeout: 5000 },
             );
