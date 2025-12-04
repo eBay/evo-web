c65cc722d8a9730bcacba1d21bfce7debd62e986
Andrew Gliga <agliga4@gmail.com>
2025-12-04 09:12:57 -0800

    chore: added 404 page

--- .marko-run/routes.d.ts
+++ .marko-run/routes.d.ts
@@ -3997,3 +3997,14 @@ declare module "../src/routes/guides/+layout.marko" {
     export const route: Run.HandlerTypeFn<Route>;
   }
 }
+
+declare module "../src/routes/+404.marko" {
+  namespace MarkoRun {
+    export { NotHandled, NotMatched, GetPaths, PostPaths, GetablePath, GetableHref, PostablePath, PostableHref, Platform };
+    export type Route = Run.Route;
+    export type Context = Run.MultiRouteContext<Route> & Marko.Global;
+    export type Handler = Run.HandlerLike<Route>;
+    /** @deprecated use `((context, next) => { ... }) satisfies MarkoRun.Handler` instead */
+    export const route: Run.HandlerTypeFn<Route>;
+  }
+}
