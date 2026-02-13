98db0fe4cc11c8fe4dd36e5455f39458fe9d15b0
LuLaValva <lukelavalva@gmail.com>
2026-02-12 22:10:38 -0800

    feat: values/checkedValues pattern

--- packages/evo-marko/src/tags/evo-tri-state-checkbox/tri-state-checkbox.stories.ts
+++ packages/evo-marko/src/tags/evo-tri-state-checkbox/tri-state-checkbox.stories.ts
@@ -1,10 +1,12 @@
 import { buildExtensionTemplate } from "../../common/storybook/utils";
 import Readme from "./README.md";
 import Component from "./index.marko";
-import defaultTemplate from "./examples/default.marko";
-import defaultCode from "./examples/default.marko?raw";
-import mixedImplementationTemplate from "./examples/mixed-implementation.marko";
-import mixedImplementationCode from "./examples/mixed-implementation.marko?raw";
+import DefaultTemplate from "./examples/default.marko";
+import DefaultCode from "./examples/default.marko?raw";
+import ControlledTemplate from "./examples/controlled.marko";
+import ControlledCode from "./examples/controlled.marko?raw";
+import ControlledCheckedValuesTemplate from "./examples/controlled-checked-values.marko";
+import ControlledCheckedValuesCode from "./examples/controlled-checked-values.marko?raw";
 import WithLabelTemplate from "./examples/with-label.marko";
 import DisabledTemplate from "./examples/disabled-with-label.marko";
 import WithLabelCode from "./examples/with-label.marko?raw";
@@ -26,10 +28,19 @@ export default {
       options: ["false", "mixed", "true"],
       type: { category: "Options" },
       description:
-        'Either "true", "false" or "mixed". Defaults to "false". Changes the checkbox state to the given one depdending on the checked state.',
+        'Either "true", "false" or "mixed". Defaults to "false". Changes the checkbox state to the given one depending on the checked state.',
       table: {
         defaultValue: {
           summary: "false",
+        },
+      },
+    },
+    checkedChange: {
+      description:
+        "Used to hoist `checked` with the [controllable](https://markojs.com/docs/explanation/controllable-components) pattern. Typically added implicitly by [`:=` bind syntax](https://markojs.com/docs/reference/language#shorthand-change-handlers-two-way-binding).",
+      table: {
+        defaultValue: {
+          summary: "(open: boolean) => void",
         },
       },
     },
@@ -40,34 +51,44 @@ export default {
         "If set, then will skip the mixed toggle when clicking on checkbox. Used if in some cases you want to toggle between all items selected or none.",
     },
     size: {
-      options: ["regular", "large"],
+      options: ["regular (default)", "large"],
       type: { category: "Options" },
       description:
-        'Either "large" or "regular". Sets the checkbox icon. Default is regular. For mweb this should be set to large. (Note: The dimensions of the checkbox will not change, but only the icon)',
+        "Sets the checkbox icon. Default is regular. (Note: The dimensions of the checkbox will not change, but only the icon)",
       table: {
         defaultValue: {
           summary: "regular",
         },
       },
     },
-    onChange: {
-      action: "on-change",
-      description: "Triggered on change",
+    values: {
+      description:
+        'Used alongside `checkedValues` for a [controllable](https://markojs.com/docs/explanation/controllable-components) pattern that aligns with [Marko\'s `checkedValue` checkbox pattern](https://markojs.com/docs/reference/native-tag#input-typeradio-and-input-typecheckbox). List of _all_ "child" checkbox values',
       table: {
-        category: "Events",
+        type: { summary: "string[]" },
+      },
+    },
+    checkedValues: {
+      description:
+        '**Must** be accompanied by `values` and `checkedValuesChange` (usually via [`:=` bind syntax](https://markojs.com/docs/reference/language#shorthand-change-handlers-two-way-binding)). Leverages [Marko\'s `checkedValue` checkbox pattern](https://markojs.com/docs/reference/native-tag#input-typeradio-and-input-typecheckbox) for "select/deselect all" behavior',
+      table: {
+        type: { summary: "string[]" },
+      },
+    },
+    checkedValuesChange: {
+      description:
+        "Used to hoist `checkedValues` with the [controllable](https://markojs.com/docs/explanation/controllable-components) pattern. Typically added implicitly by [`:=` bind syntax](https://markojs.com/docs/reference/language#shorthand-change-handlers-two-way-binding).",
+      table: {
         defaultValue: {
-          summary: "{ originalEvent, value, checked }",
+          summary: "(checkedValues: string[]) => void",
         },
       },
     },
-    onFocus: {
-      action: "on-focus",
-      description: "Triggered on focus",
+    "all <input> attributes": {
+      description:
+        "All attributes and event handlers from the [native HTML `<input>` tag](https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/input) may be passed through, and its Marko [change handlers](https://markojs.com/docs/reference/native-tag#input-valuechange-checkedchange-checkedvaluechange)",
       table: {
-        category: "Events",
-        defaultValue: {
-          summary: "{ originalEvent, value }",
-        },
+        category: "<input> attributes",
       },
     },
   },
@@ -80,9 +101,14 @@ export const WithLabel = buildExtensionTemplate(
 
 export const Disabled = buildExtensionTemplate(DisabledTemplate, DisabledCode);
 
-export const MixedImplementation = buildExtensionTemplate(
-  mixedImplementationTemplate,
-  mixedImplementationCode,
+export const ControlledCheckedValues = buildExtensionTemplate(
+  ControlledCheckedValuesTemplate,
+  ControlledCheckedValuesCode,
 );
 
-export const Isolated = buildExtensionTemplate(defaultTemplate, defaultCode);
+export const Controlled = buildExtensionTemplate(
+  ControlledTemplate,
+  ControlledCode,
+);
+
+export const Isolated = buildExtensionTemplate(DefaultTemplate, DefaultCode);
