a7ee58eb8e40e8f72ce8e4dd05c94ffd53344e93
HenriqueLimas <henrique.ramos.limas@gmail.com>
2026-05-08 18:34:39 -0700

    refactor(evo-react): remove unnecessary overloads from useRefTee

--- packages/evo-react/src/utils/use-ref-tee.ts
+++ packages/evo-react/src/utils/use-ref-tee.ts
@@ -1,21 +1,21 @@
 import { useCallback, useRef } from "react";
 import type { Ref } from "react";
 
-export function useRefTee<T>(ref: Ref<T> | undefined, initialValue: T): readonly [Ref<T>, React.RefObject<T>];
-export function useRefTee<T>(ref: Ref<T | null> | undefined, initialValue?: null): readonly [Ref<T | null>, React.RefObject<T | null>];
-export function useRefTee<T>(ref: Ref<T> | undefined, initialValue?: T) {
-  const internalRef = useRef<T>(initialValue as T);
+export function useRefTee<T>(ref: Ref<T> | undefined, initialValue: T) {
+  const internalRef = useRef<T>(initialValue);
 
   const teeRef = useCallback(
     (node: T) => {
       internalRef.current = node;
-      if (typeof ref === "function") {
-        ref(node);
-      } else if (ref) {
-        (ref as React.MutableRefObject<T>).current = node;
+
+      if (ref) {
+        if (typeof ref === "function") {
+          ref(node);
+        } else {
+          ref.current = node;
+        }
       }
     },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     [ref],
   );
 
