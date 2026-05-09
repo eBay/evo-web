25b3e7f973090cce06db0d17ae2fedea774989c2
HenriqueLimas <henrique.ramos.limas@gmail.com>
2026-05-08 18:22:46 -0700

    refactor(evo-react): rename externalId to initialId in useControllableId

--- packages/evo-react/src/utils/use-controllable-id.ts
+++ packages/evo-react/src/utils/use-controllable-id.ts
@@ -1,7 +1,7 @@
 import { useId, useState } from "react";
 
-export function useControllableId(externalId?: string) {
+export function useControllableId(initialId?: string) {
   const generatedId = useId();
-  const [id, setId] = useState(externalId ?? generatedId);
+  const [id, setId] = useState(initialId ?? generatedId);
   return [id, setId] as const;
 }
