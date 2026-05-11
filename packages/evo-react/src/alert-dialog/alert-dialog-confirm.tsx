d3ce7e2a2d06c127f6b2e02c7bbb8eff59ba3be1
HenriqueLimas <henrique.ramos.limas@gmail.com>
2026-05-11 09:12:11 -0700

    refactor(evo-react): use useRefTee and useCallback in EvoAlertDialogConfirm

--- packages/evo-react/src/alert-dialog/alert-dialog-confirm.tsx
+++ packages/evo-react/src/alert-dialog/alert-dialog-confirm.tsx
@@ -1,19 +1,25 @@
-import type { MouseEvent } from "react";
 import type { EvoAlertDialogConfirmProps } from "./types";
 import { useAlertDialogContext } from "./context";
-import { EvoButton } from "../button/button";
+import { useRefTee } from "../utils/use-ref-tee";
+import { EvoButton, type NativeButtonProps } from "../button";
+import { useCallback } from "react";
 
 export function EvoAlertDialogConfirm({
+  ref,
   onClick,
   children,
   ...rest
 }: EvoAlertDialogConfirmProps) {
   const { mainId, onConfirmClick } = useAlertDialogContext();
+  const [buttonRef] = useRefTee<HTMLButtonElement | null>(ref);
 
-  function handleClick(e: MouseEvent<HTMLButtonElement>) {
-    onConfirmClick();
-    onClick?.(e);
-  }
+  const handleClick: NonNullable<NativeButtonProps["onClick"]> = useCallback(
+    (e) => {
+      onConfirmClick();
+      onClick?.(e);
+    },
+    [onClick, onConfirmClick],
+  );
 
   return (
     <div className="dialog__footer">
@@ -21,6 +27,7 @@ export function EvoAlertDialogConfirm({
         {...rest}
         autoFocus
         ref={(button) => {
+          buttonRef(button);
           // React doesn't set autofocus on the DOM element inside a dialog
           // https://github.com/facebook/react/issues/23301
           if (button) button.autofocus = true;
