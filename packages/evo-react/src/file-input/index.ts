45e7bb9b5c3171fa6948353390e99fe20588e81d
Henrique Limas <henrique.ramos.limas@gmail.com>
2026-08-12 17:42:28 -0700

    feat(evo-react): add file input (#883)

---
+++ packages/evo-react/src/file-input/index.ts
@@ -0,0 +1,10 @@
+export { EvoFileInput } from "./file-input";
+export { EvoFileInputHeader } from "./file-input-header";
+export { EvoFileInputDescription } from "./file-input-description";
+export { EvoFileInputCTA } from "./file-input-cta";
+export type {
+  EvoFileInputProps,
+  EvoFileInputHeaderProps,
+  EvoFileInputDescriptionProps,
+  EvoFileInputCTAProps,
+} from "./types";
